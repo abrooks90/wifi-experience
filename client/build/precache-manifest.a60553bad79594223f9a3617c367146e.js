025cec553024c1ef6e08fbda21914418518f7e5e
Andrew Brooks <andrew.brooks.aob@gmail.com>
2019-05-03 19:20:27 -0400

    Redid the package.json files so it's easy to run npm install.
    Added detailed instructions for setting up the server and raspberry pi.

---
+++ client/build/precache-manifest.a60553bad79594223f9a3617c367146e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b5323b5f41f9ae4825a2b7553e223b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97e711e0585207800c4f",
+    "url": "/static/js/2.a74064cc.chunk.js"
+  },
+  {
+    "revision": "e8a7b6f379716e88da5e",
+    "url": "/static/js/main.5fdd5819.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
